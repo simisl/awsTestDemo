3d34d2d93c9e2babfe7b0c64683e327cc2ada3d1
SIMI <simisann@gmail.com>
2025-08-19 03:12:00 +0400

    chh

--- backend/src/main/java/com/example/backend/config/WebConfig.java
+++ backend/src/main/java/com/example/backend/config/WebConfig.java
@@ -1,21 +1,21 @@
-package com.example.backend.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        // Map root and all unmatched routes to index.html
-        registry.addViewController("/{spring:\\w+}")
-                .setViewName("forward:/index.html");
-        registry.addViewController("/**/{spring:\\w+}")
-                .setViewName("forward:/index.html");
-        registry.addViewController("/")
-                .setViewName("forward:/index.html");
-    }
-}
-
-
+//package com.example.backend.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addViewControllers(ViewControllerRegistry registry) {
+//        // Map root and all unmatched routes to index.html
+//        registry.addViewController("/{spring:\\w+}")
+//                .setViewName("forward:/index.html");
+//        registry.addViewController("/**/{spring:\\w+}")
+//                .setViewName("forward:/index.html");
+//        registry.addViewController("/")
+//                .setViewName("forward:/index.html");
+//    }
+//}
+//
+//
